7b05317821c13e613d0f9e1559e1651e2cfe0619
AlfianRhmn <dvoraktheqwert@gmail.com>
2025-05-20 09:36:23 +0700

    Add full image in gallery + seperate css and js

---
+++ script.js
@@ -0,0 +1,13 @@
+  function openLightbox(img) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'flex';
+    document.body.classList.add('lightbox-open');
+  }
+
+  function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+    document.body.classList.remove('lightbox-open');
+  }
